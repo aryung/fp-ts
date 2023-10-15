03ed7cdc0622ad8bdc2f0b8e2bcfc16083802b53
achen@iMac <achen@cirrusled.com>
2023-10-15 11:06:16 +0800

    fix: modify tool

--- src/tool.ts
+++ src/tool.ts
@@ -1,9 +1,9 @@
-export type None = { _type: "none" };
-export type Some<T> = { _type: "some"; value: T };
+export type None = { _type: 'none' };
+export type Some<T> = { _type: 'some'; value: T };
 export type Option<T> = None | Some<T>;
 
-export const none: None = { _type: "none" };
-export const some = <T>(value: T): Some<T> => ({ _type: "some", value });
+export const none: None = { _type: 'none' };
+export const some = <T>(value: T): Some<T> => ({ _type: 'some', value });
 
 export function optionalCatch<T>(fn: () => T): Option<T> {
   try {
@@ -13,12 +13,8 @@ export function optionalCatch<T>(fn: () => T): Option<T> {
   }
 }
 
-export function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
-
 export async function optionResolve<T>(
-  promise: Promise<T>,
+  promise: Promise<T>
 ): Promise<Option<T>> {
   // try {
   //   return some(await promise);
@@ -43,26 +39,26 @@ export function toOptional<I, O extends I>(fn: (i: I) => i is O) {
 }
 
 export const optionalDefined = toOptional(
-  <T>(arg: T | undefined | null): arg is T => arg != null,
+  <T>(arg: T | undefined | null): arg is T => arg != null
 );
 
 // unwrap
 export function unwrap<T>(option: Option<T>): T {
-  if (option._type === "none") {
-    throw new Error("Cannot unwrap a Option");
+  if (option._type === 'none') {
+    throw new Error('Cannot unwrap a Option');
   }
   return option.value;
 }
 
 export function unwrapOr<T>(option: Option<T>, defaultValue: T): T {
-  if (option._type === "none") {
+  if (option._type === 'none') {
     return defaultValue;
   }
   return option.value;
 }
 
 export function unwrapExpect<T>(option: Option<T>, message: string): T {
-  if (option._type === "none") {
+  if (option._type === 'none') {
     throw new Error(message);
   }
   return option.value;
