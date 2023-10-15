431d70064b9a4b192a65e7cb1d9ed78928c1aa74
aryung <aryung@gmail.com>
2023-10-15 14:44:07 +0800

    doc: add example for use

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-import { optionalDefined, unwrapExpect, unwrapOr } from "./tool";
-// sample
-function getTimeDiff(arr: Array<Date>): number {
-  const start = optionalDefined(arr[0]);
-  const end = optionalDefined(arr[1]);
+// import { optionalDefined, unwrapExpect, unwrapOr } from "./tool";
+// // sample
+// function getTimeDiff(arr: Array<Date>): number {
+//   const start = optionalDefined(arr[0]);
+//   const end = optionalDefined(arr[1]);
+//
+//   const startVall = unwrapExpect(start, "range must have a string").valueOf();
+//   const endVal = unwrapOr(end, new Date()).valueOf();
+//   return endVal - startVall;
+// }
+//
+// console.log(getTimeDiff([new Date()]));
 
-  const startVall = unwrapExpect(start, "range must have a string").valueOf();
-  const endVal = unwrapOr(end, new Date()).valueOf();
-  return endVal - startVall;
-}
+import { Some, map, getOrElse, Option } from './util';
 
-console.log(getTimeDiff([new Date()]));
+// const demo1: Some<number> = {
+//   type: 'None',
+//   // value:1 ,
+// };
+
+const demo1: Option<number> = {
+  type: 'None',
+  // value: 1,
+};
+
+const demo2 = map(demo1, (v: number): number => v + 1);
+// const demo3 = getOrElse(demo1, 0);
+
+console.log({ demo1, demo2 });
